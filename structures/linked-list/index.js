bd3034a4cfc285a6d6d88a4bbfbad2d1fa2428dd
stephanrotolante <stephanrotolante@gmail.com>
2021-01-16 22:44:06 -0500

    commit

--- structures/linked-list/index.js
+++ structures/linked-list/index.js
@@ -2,6 +2,9 @@
 var myFunction = function (value) {
     return value || "default value";
 };
+var myFunction2 = function (value) {
+    return value || "default value";
+};
 module.exports = {
     myFunction: myFunction,
 };
--- structures/linked-list/index.ts
+++ structures/linked-list/index.ts
@@ -1,6 +1,9 @@
 const myFunction = (value: string): string => {
   return value || "default value";
 };
+const myFunction2 = (value: string): string => {
+  return value || "default value";
+};
 module.exports = {
   myFunction,
 };
